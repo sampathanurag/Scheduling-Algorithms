e6b2d6423fd79466b6f05a0bc57a8275d2780c3c
Anurag A S <sampathanurag3@gmail.com>
2019-12-14 11:26:42 +0530

    Refactored code for SJF, Added a few comments

--- Shortest Job First/add_process.py
+++ Shortest Job First/add_process.py
@@ -1,14 +1,28 @@
-# program to add a process by specifying the process ID and task time
-# function to display the id and the time remaining can be called by display()
+# Script to add a process by specifying the process ID and task time
+# The function to display the id and the time remaining can be accomplished by invoking the function display()
 
 
 class Process:
+    """"
+    Class to create process objects with various attributes
+    """
     def __init__(self, Id, arrivalTime, taskTime, log=False):
+        """
+        Initialization function
+        :param Id: Process Id
+        :param arrivalTime: Arrival time of the process into the system
+        :param taskTime: The time taken for completion of the process
+        :param log: Parameter set when the process is recognized and added to the process queue
+        """
         self.Id = Id
         self.taskTime = taskTime
         self.arrivalTime = arrivalTime
         self.log = log
 
     def display(self):
+        """
+        Function to display the process in a neat understandable format
+        :return: None
+        """
         print("Process ID:", self.Id, "| Arrival Time:", self.arrivalTime, "| Task Time Remaining:",
               self.taskTime, "| Log:", self.log)
